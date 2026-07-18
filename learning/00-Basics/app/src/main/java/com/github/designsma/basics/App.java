1d78aa9411a4a1a0d28e2bd51f2ef34a1752b31d
Marco Antonio C <designsma.contact@gmail.com>
2026-07-18 11:47:42 -0600

    Implement main method in App.java with user input handling and option selection

--- learning/00-Basics/app/src/main/java/com/github/designsma/basics/App.java
+++ learning/00-Basics/app/src/main/java/com/github/designsma/basics/App.java
@@ -1,11 +1,33 @@
 package com.github.designsma.basics;
+import java.util.Scanner;
+import java.io.BufferedReader;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
+    public static void main( String[] args)
+    {
+        Scanner sc = new Scanner(System.in); // read from keyboard
+
+        System.out.println("Hello, World!\nSelect an option!");
+        System.out.println("1. IO\n2. Other");
+
+        int opt = sc.nextInt();
+        sc.close();
+
+        switch (opt) {
+            case 1:
+                io_func();
+                break;
+            case 2:
+                break;
+            default:
+                throw new AssertionError("No valid option was introduced.");
+        }
+
+        
     }
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+    private static void io_func()
+    {
+        System.out.println("...");
     }
 }
