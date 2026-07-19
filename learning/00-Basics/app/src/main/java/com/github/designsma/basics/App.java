313bf611717d651fb24c28151d0d93641ffba90d
Marco Antonio C <designsma.contact@gmail.com>
2026-07-18 21:42:00 -0600

    Enhance App functionality with guessing game feature and improved error handling

--- learning/00-Basics/app/src/main/java/com/github/designsma/basics/App.java
+++ learning/00-Basics/app/src/main/java/com/github/designsma/basics/App.java
@@ -1,26 +1,33 @@
 package com.github.designsma.basics;
 import java.util.Scanner;
-import java.io.BufferedReader;
 
 public class App {
+    public String getGreeting()
+    {
+        return "Hello there!";
+    }
     public static void main( String[] args)
     {
         Scanner sc = new Scanner(System.in); // read from keyboard
 
         System.out.println("Hello, World!\nSelect an option!");
-        System.out.println("1. IO\n2. Other");
+        System.out.println("1. IO\n2. Guessing Game");
 
         int opt = sc.nextInt();
-        sc.close();
-
-        switch (opt) {
-            case 1:
-                io_func();
-                break;
-            case 2:
-                break;
-            default:
-                throw new AssertionError("No valid option was introduced.");
+        try {
+            switch (opt) {
+                case 1:
+                    io_func();
+                    break;
+                case 2:
+                    guessing_game(sc);
+                    break;
+                default:
+                    throw new AssertionError("No valid option was introduced.");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Something went wrong. Please run the program again.");
         }
 
         
@@ -29,5 +36,39 @@ public class App {
     private static void io_func()
     {
         System.out.println("...");
+    }
+
+    private static void guessing_game(Scanner sc)
+    {
+        byte number = (byte) (1+ Math.random()*99); // random number between 1 and 100
+        byte tries = 5;
+
+        System.out.println("Guess a number between 1 and 100. You have " + tries + " tries.");
+
+        while (tries>0)
+        {
+            System.out.println("Guess a number: ");
+            byte guess = (byte) sc.nextInt();
+
+            if (guess == number)
+            {
+                
+                System.out.println(
+                        "Congratulations! You guessed the correct number.");
+                sc.close();
+                
+                // Exit function if guessed correctly
+                return;
+            }
+            else System.out.println("The number is " + (number > guess ? "higher" : "lower") + " than " + guess);
+
+            --tries;
+        }
+
+        // If the user runs out of attempts
+        System.out.println(
+            "You've exhausted all attempts. The correct number was: "
+            + number);
+        sc.close();
     }
 }
