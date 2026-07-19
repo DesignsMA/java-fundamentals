6bb96974923763b0305b27ca204cfca0da1f64f4
Marco Antonio C <designsma.contact@gmail.com>
2026-07-19 16:32:36 -0600

    Implement preparationTimeInMinutes and totalTimeInMinutes methods in Lasagna class

--- exercism/java/lasagna/src/main/java/Lasagna.java
+++ exercism/java/lasagna/src/main/java/Lasagna.java
@@ -1,16 +1,21 @@
 public class Lasagna {
-    // TODO: define the 'expectedMinutesInOven()' method
     public int expectedMinutesInOven()
     {
         return 40;
     }
 
-    // TODO: define the 'remainingMinutesInOven()' method
     public int remainingMinutesInOven(int ovenTime)
     {
         return this.expectedMinutesInOven()%ovenTime;
     }
-    // TODO: define the 'preparationTimeInMinutes()' method
 
-    // TODO: define the 'totalTimeInMinutes()' method
+    public int preparationTimeInMinutes(int layers)
+    {
+        return layers<<1;
+    }
+
+    public int totalTimeInMinutes(int layers, int ovenTime)
+    {
+        return preparationTimeInMinutes(layers)+ovenTime;
+    }
 }
