bf5672451deb10f4cfadb19d2395e9cb84bdb299
Marco Antonio C <designsma.contact@gmail.com>
2026-07-21 15:19:59 -0600

    Implement JedliksToyCar methods for distance and battery display, and driving functionality

--- exercism/java/jedliks-toy-car/src/main/java/JedliksToyCar.java
+++ exercism/java/jedliks-toy-car/src/main/java/JedliksToyCar.java
@@ -1,17 +1,29 @@
 public class JedliksToyCar {
+    private int distance;
+    private int battery;
+
+    public JedliksToyCar() {
+        this.distance = 0;
+        this.battery = 100;
+    }
+
     public static JedliksToyCar buy() {
-        throw new UnsupportedOperationException("Please implement the (static) JedliksToyCar.buy()  method");
+        return new JedliksToyCar();
     }
 
     public String distanceDisplay() {
-        throw new UnsupportedOperationException("Please implement the JedliksToyCar.distanceDisplay()  method");
+        return "Driven " + distance + " meters";
     }
 
     public String batteryDisplay() {
-        throw new UnsupportedOperationException("Please implement the JedliksToyCar.batteryDisplay()  method");
+        if (battery <= 0) return  "Battery empty";
+        return "Battery at " + battery + "%";
     }
 
     public void drive() {
-        throw new UnsupportedOperationException("Please implement the JedliksToyCar.drive()  method");
+        if (battery <= 0) return;
+        
+        distance+=20;
+        battery-=1;
     }
 }
