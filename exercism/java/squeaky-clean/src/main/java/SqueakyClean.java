283ce69479e74f575c0af8adcca4da6d3b2cb3e7
Marco Antonio C <designsma.contact@gmail.com>
2026-07-21 15:01:53 -0600

    Implement SqueakyClean.clean() method for string formatting and leet speak conversion

--- exercism/java/squeaky-clean/src/main/java/SqueakyClean.java
+++ exercism/java/squeaky-clean/src/main/java/SqueakyClean.java
@@ -1,5 +1,37 @@
+import java.util.Map;
+
 class SqueakyClean {
+    private static final Map<Character, Character> LEET_SPEAK_MAP = Map.of(
+        '4', 'a',
+        '3', 'e',
+        '0', 'o',
+        '1', 'l',
+        '7', 't'
+    );
+
     static String clean(String identifier) {
-        throw new UnsupportedOperationException("Please implement the (static) SqueakyClean.clean() method");
+        StringBuilder sb = new StringBuilder();
+        boolean capitalizeNext = false;
+
+        for (char ch : identifier.toCharArray()) {
+            if (ch == ' ') {
+                sb.append('_');
+            } else if (ch == '-') {
+                capitalizeNext = true;
+            } else if (Character.isLetter(ch)) {
+                if (capitalizeNext) {
+                    sb.append(Character.toUpperCase(ch));
+                    capitalizeNext = false;
+                } else {
+                    sb.append(ch);
+                }
+            } else {
+                Character replacement = LEET_SPEAK_MAP.get(ch);
+                if (replacement != null) {
+                    sb.append(replacement);
+                }
+            }
+        }
+        return sb.toString();
     }
 }
