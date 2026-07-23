98d5a685f68a799cebf683f32c6615dc6b81bda2
Marco Antonio C <designsma.contact@gmail.com>
2026-07-23 12:14:25 -0600

    Implement calculate method in CalculatorConundrum to handle basic arithmetic operations and input validation

--- exercism/java/calculator-conundrum/src/main/java/CalculatorConundrum.java
+++ exercism/java/calculator-conundrum/src/main/java/CalculatorConundrum.java
@@ -1,5 +1,27 @@
 class CalculatorConundrum {
     public String calculate(int operand1, int operand2, String operation) {
-        throw new UnsupportedOperationException("Please implement the CalculatorConundrum.calculate() method");
+        if (operation == null) {
+            throw new IllegalArgumentException("Operation cannot be null");
+        } else if (operation.equals("")) {
+            throw new IllegalArgumentException("Operation cannot be empty");
+        }
+
+        switch (operation) {
+            case "+":
+                return operand1 + " + " + operand2 + " = " + (operand1+operand2);
+            case "-":
+                return operand1 + " - " + operand2 + " = " + (operand1-operand2);
+            case "*":
+                return operand1 + " * " + operand2 + " = " + (operand1*operand2);
+            case "/":
+                try {
+                    return operand1 + " / " + operand2 + " = " + (operand1/operand2);
+                } catch (ArithmeticException e) {
+                    throw new IllegalOperationException("Division by zero is not allowed", e);
+                }
+
+            default:
+                throw new IllegalOperationException("Operation '" + operation + "' does not exist");
+        }
     }
 }
