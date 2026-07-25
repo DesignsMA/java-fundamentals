788fc43952b1429330ebec25be41eba6e8e75c29
Marco Antonio C <designsma.contact@gmail.com>
2026-07-24 20:43:18 -0600

    Implement AppointmentScheduler methods for scheduling, status checks, and descriptions

--- exercism/java/booking-up-for-beauty/src/main/java/AppointmentScheduler.java
+++ exercism/java/booking-up-for-beauty/src/main/java/AppointmentScheduler.java
@@ -1,24 +1,36 @@
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 class AppointmentScheduler {
     public LocalDateTime schedule(String appointmentDateDescription) {
-        throw new UnsupportedOperationException("Please implement the AppointmentScheduler.schedule() method");
+        return LocalDateTime.parse(
+            appointmentDateDescription,
+            DateTimeFormatter.ofPattern("M/dd/yyyy HH:mm:ss")
+        );
     }
 
     public boolean hasPassed(LocalDateTime appointmentDate) {
-        throw new UnsupportedOperationException("Please implement the AppointmentScheduler.hasPassed() method");
+        return LocalDateTime.now().isAfter(appointmentDate); 
     }
 
     public boolean isAfternoonAppointment(LocalDateTime appointmentDate) {
-        throw new UnsupportedOperationException("Please implement the AppointmentScheduler.isAfternoonAppointment() method");
+        int hour = appointmentDate.getHour();
+        return  hour >= 12 && hour < 18;
     }
 
     public String getDescription(LocalDateTime appointmentDate) {
-        throw new UnsupportedOperationException("Please implement the AppointmentScheduler.getDescription() method");
+        return appointmentDate.format(
+            DateTimeFormatter.ofPattern(
+                "'You have an appointment on' EEEE, MMMM d, yyyy, 'at' h:mm a.", 
+                Locale.US
+            )
+        );
     }
 
     public LocalDate getAnniversaryDate() {
-        throw new UnsupportedOperationException("Please implement the AppointmentScheduler.getAnniversaryDate() method");
+        return LocalDate.now().withMonth(9)
+                              .withDayOfMonth(15);
     }
 }
