86149f1b57258d4889689d7323692ddae0d9b5de
Marco Antonio C <designsma.contact@gmail.com>
2026-07-26 20:22:13 -0600

    Implement methods in GottaSnatchEmAll class for collection management

--- exercism/java/gotta-snatch-em-all/src/main/java/GottaSnatchEmAll.java
+++ exercism/java/gotta-snatch-em-all/src/main/java/GottaSnatchEmAll.java
@@ -1,25 +1,43 @@
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 class GottaSnatchEmAll {
 
     static Set<String> newCollection(List<String> cards) {
-        throw new UnsupportedOperationException("Please implement the (static) GottaSnatchEmAll.newCollection() method");
+        return new HashSet<>(cards);
     }
 
     static boolean addCard(String card, Set<String> collection) {
-        throw new UnsupportedOperationException("Please implement the (static) GottaSnatchEmAll.addCard() method");
+        return collection.add(card);
     }
 
     static boolean canTrade(Set<String> myCollection, Set<String> theirCollection) {
-        throw new UnsupportedOperationException("Please implement the (static) GottaSnatchEmAll.canTrade() method");
+        return !myCollection.containsAll(theirCollection) && !theirCollection.containsAll(myCollection);
     }
 
-    static Set<String> commonCards(List<Set<String>> collections) {
-        throw new UnsupportedOperationException("Please implement the (static) GottaSnatchEmAll.commonCards() method");
+    static Set<String> commonCards(List<Set<String>> collections)
+     {
+        Set<String> commonCardSet = new HashSet<>();
+        
+        if (collections.isEmpty()) {
+            return commonCardSet;
+        }
+        else {
+            commonCardSet.addAll(collections.get(0));
+        }
+
+        for (Set<String> collection : collections) {
+            commonCardSet.retainAll(collection);
+        }
+        return commonCardSet;
     }
 
     static Set<String> allCards(List<Set<String>> collections) {
-        throw new UnsupportedOperationException("Please implement the (static) GottaSnatchEmAll.allCards() method");
+        Set<String> allCardSet = new HashSet<>();
+        for (Set<String> collection : collections) {
+            allCardSet.addAll(collection);
+        }
+        return allCardSet;
     }
 }
