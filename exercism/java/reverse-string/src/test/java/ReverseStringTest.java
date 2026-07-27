e62114e4affca317eb2a8d1d64b52834b9dda9dd
Marco Antonio C <designsma.contact@gmail.com>
2026-07-27 16:46:44 -0600

    Implement reverse method in ReverseString class and enable corresponding tests

--- exercism/java/reverse-string/src/test/java/ReverseStringTest.java
+++ exercism/java/reverse-string/src/test/java/ReverseStringTest.java
@@ -1,8 +1,6 @@
-import org.junit.jupiter.api.Disabled;
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 public class ReverseStringTest {
 
@@ -12,35 +10,30 @@ public class ReverseStringTest {
         assertThat(new ReverseString().reverse("")).isEqualTo("");
     }
 
-    @Disabled("Remove to run test")
     @Test
     @DisplayName("a word")
     public void testAWord() {
         assertThat(new ReverseString().reverse("robot")).isEqualTo("tobor");
     }
 
-    @Disabled("Remove to run test")
     @Test
     @DisplayName("a capitalized word")
     public void testACapitalizedWord() {
         assertThat(new ReverseString().reverse("Ramen")).isEqualTo("nemaR");
     }
 
-    @Disabled("Remove to run test")
     @Test
     @DisplayName("a sentence with punctuation")
     public void testASentenceWithPunctuation() {
         assertThat(new ReverseString().reverse("I'm hungry!")).isEqualTo("!yrgnuh m'I");
     }
 
-    @Disabled("Remove to run test")
     @Test
     @DisplayName("a palindrome")
     public void testAPalindrome() {
         assertThat(new ReverseString().reverse("racecar")).isEqualTo("racecar");
     }
 
-    @Disabled("Remove to run test")
     @Test
     @DisplayName("an even-sized word")
     public void testAnEvenSizedWord() {
